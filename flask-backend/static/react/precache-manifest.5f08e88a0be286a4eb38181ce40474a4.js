e1570ce5b39820417cff88349c1d711e04d1d0ba
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-02-17 22:15:31 -0500

    first commit

---
+++ flask-backend/static/react/precache-manifest.5f08e88a0be286a4eb38181ce40474a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf979bdda550167231a4c1e0a02c5343",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "35df0ca4f243fb361c8b",
+    "url": "/static/react/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "44db4f6cbe10952ca77b",
+    "url": "/static/react/js/2.9a345854.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/react/js/2.9a345854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35df0ca4f243fb361c8b",
+    "url": "/static/react/js/main.1735db43.chunk.js"
+  },
+  {
+    "revision": "165304c935f15a4707af",
+    "url": "/static/react/js/runtime-main.7ca781aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
